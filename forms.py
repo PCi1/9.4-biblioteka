1e2121c9689ba934645535ce903efe75c0bdf493
PCi1 <87982813+PCi1@users.noreply.github.com>
2021-10-19 23:44:16 +0200

    Add files via upload

---
+++ forms.py
@@ -0,0 +1,35 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, BooleanField, IntegerField, SelectField, HiddenField
+from wtforms.validators import DataRequired
+import json
+from models import books
+
+choices_list=[]
+# wiem że ten kod nie powinien się to znajdować ale z jakiegoś powodu wstawienie tego kodu do models powoduje że lista autorów jest aktualizowana tylko co restart servera,
+# to co zrobiłem poniżej to jedyny sposób który znajazłem na to by to działało tak jak chcę
+def reload_authors():
+    try:
+        with open("authors.json", 'r') as f:
+            authors_list = json.load(f)
+    except FileNotFoundError:
+        authors_list = []
+    choices_list.clear()
+    for author in authors_list:
+        choices_list.append(f"{author.get('first_name')} {author.get('last_name')}")
+reload_authors()
+
+
+class BookForm(FlaskForm):
+# chciałem tutaj dodać "id=hiddenfield("id", default=books.all()[-1]["id"]+1)" ale wartośc zmieniała się tylko przy restarcie servera
+# np. dodanie 3 nowych książek po załadowaniu strony prowadziło do tego że wszystkie 3 miały to samo ID
+# próbowałem to rozwiązać czymś podobnym do "reload_authors()"ale też nie działało
+    id=HiddenField('id', default=int(books.last_id()) +1)
+    title=StringField("Tytuł", validators=[DataRequired()])
+    author=SelectField("Autor",choices = choices_list, validators=[DataRequired()])
+    read=BooleanField("Czy przeczytana")
+    length=IntegerField("Ilość stron", validators=[DataRequired()])
+
+class AuthorForm(FlaskForm):
+    first_name=StringField("Imie", validators=[DataRequired()])
+    last_name=StringField('Nazwisko', validators=[DataRequired()])
+
---
+++ models.py
@@ -0,0 +1,71 @@
+import json
+
+
+class Books:
+    def __init__(self):
+        try:
+            with open("books.json", "r") as f:
+                self.books = json.load(f)
+        except FileNotFoundError:
+            self.books = []
+
+    def all(self):
+        return self.books
+
+    def get(self, id):
+        return self.books[id]
+
+    def create(self, data):
+        data.pop('csrf_token')
+        self.books.append(data)
+
+    def save_all(self):
+        with open("books.json", "w") as f:
+            json.dump(self.books, f)
+
+    def update(self, id, data):
+        data.pop('csrf_token')
+        self.books[id] = data
+        self.save_all()
+
+    def last_id(self):
+        if self.books:
+            last_id = self.books[-1].get('id')
+        else:
+            last_id = 0
+        return last_id
+
+
+books = Books()
+
+class Authors:
+    def __init__(self):
+        try:
+            with open("authors.json", 'r') as f:
+                self.authors = json.load(f)
+        except FileNotFoundError:
+            self.authors = []
+    
+    def all(self):
+        return self.authors
+
+    def create(self, data):
+        data.pop('csrf_token')
+        self.authors.append(data)
+    
+    def save_all(self):
+        with open("authors.json", "w") as f:
+            json.dump(self.authors, f)
+    
+    def choice_list(self):
+        try:
+            with open("authors.json", 'r') as f:
+                self.authors = json.load(f)
+        except FileNotFoundError:
+            self.authors = []
+        choices_list = []
+        for author in authors.all():
+            choices_list.append(f"{author.get('first_name')} {author.get('last_name')}")
+        return choices_list
+
+authors = Authors()
